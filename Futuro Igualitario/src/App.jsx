77d7bedc7ab0a50635df2669dd63e960fe65b533
JulioAlb453 <142369736+JulioAlb453@users.noreply.github.com>
2024-07-02 11:44:48 -0600

    update logo

--- Futuro Igualitario/src/App.jsx
+++ Futuro Igualitario/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./assets/pages/Home";
 import LoginForm from "./assets/pages/LoginForm";
-import Navbar from "./assets/Components/Navbar";
 import AgendarCitas from "./assets/pages/AgendarCitas";
 import "./navbar.css";
 function App() {
